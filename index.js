f94d3893bf2f36f69ef192937f36c97f335f24b4
Colton G. Rushton <colton51919@gmail.com>
2019-02-28 13:36:27 -0400

    Update index.js
    Use a simpler approach than earlier to harvest the version number and make it available to `<toolsvariablename>.version`.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const RegexWorld = require("regexworld");
 
 // Functions
 module.exports = {
-
+    version: require('./package.json').version,
     embed: function(channel, message, deleteTimer, hexColor) {
         channel.send({
             embed: {
